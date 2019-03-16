e722edf81c8890885eff18ba0fb3d285796abd12
Alessandro Genova <ales.genova@gmail.com>
2019-03-16 19:44:25 -0400

    Updates

---
+++ precache-manifest.212ca39b672bb7630d773ee0de56d076.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "292a58fac6b30b3c9d4306112f70ef01",
+    "url": "/pitch-detection-app/static/media/stop.292a58fa.svg"
+  },
+  {
+    "revision": "7674a1b6339f05fca47f1dda9b3f7880",
+    "url": "/pitch-detection-app/static/media/play.7674a1b6.svg"
+  },
+  {
+    "revision": "43edf1ebb7b63993e784",
+    "url": "/pitch-detection-app/static/js/runtime~main.bc85d236.js"
+  },
+  {
+    "revision": "6a849baffdb4641dde69",
+    "url": "/pitch-detection-app/static/js/main.439c75ee.chunk.js"
+  },
+  {
+    "revision": "868745d3a7c6af7326fe",
+    "url": "/pitch-detection-app/static/js/3.eb3bfa99.chunk.js"
+  },
+  {
+    "revision": "c7f61841e64a67e06fd5",
+    "url": "/pitch-detection-app/static/js/2.b0b9fa78.chunk.js"
+  },
+  {
+    "revision": "868745d3a7c6af7326fe",
+    "url": "/pitch-detection-app/static/css/3.7d73143d.chunk.css"
+  },
+  {
+    "revision": "ee07f9c2945a810f5c68a1ea25ae0d91",
+    "url": "/pitch-detection-app/index.html"
+  },
+  {
+    "revision": "868745d3a7c6af7326fe",
+    "url": "/pitch-detection-app/9815cf1d6d7663c82270.module.wasm"
+  }
+];
